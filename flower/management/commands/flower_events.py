555aa26e2e8be36588dbe2f78cb606df4d1f49bc
alexsilva <geniofuturo@gmail.com>
2019-07-05 12:28:47 -0300

    Protect enable_events.

--- flower/management/commands/flower_events.py
+++ flower/management/commands/flower_events.py
@@ -1,11 +1,13 @@
 # coding=utf-8
+import logging
+
 import gevent.monkey
 
 gevent.monkey.patch_all()
 
 from django.core.management import BaseCommand
 
-from flower.events import Events
+from flower.events import Events, logger
 from flower.options import options as settings
 
 
@@ -13,5 +15,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         events = Events(settings.app, settings)
-        events.enable_events()
+        try:
+            settings.app.control.enable_events()
+        except Exception as e:
+            logger.debug("Failed to enable events: '%s'", e)
         events.run()
